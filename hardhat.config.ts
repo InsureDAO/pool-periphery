1a0d383afcdafb502c67f2c2fc3ede1664429d0a
Shun Oikawa <oishun@Shuns-MacBook-Pro.local>
2022-04-20 22:47:47 +0900

    Shibuya deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,10 +25,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.10",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    shibuya: {
+      url: process.env.SHIBUYA_URL,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
---
+++ scripts/Shibuya/deployments.ts
@@ -0,0 +1,23 @@
+const USDCAddress = "0x178e56d3e3961210d5760bb8125b64c9661a3428";
+const OwnershipAddress = "0x2051Ec11ca029DF3C09eCb3855EeCcB98D3C1433";
+const RegistryAddress = "0x55478DD610e3d0f31238E6C47CBD2fa74549e7Ba";
+const FactoryAddress = "0xCaFC83043BDfb2359DC6bc48c9A96C50A9Af9bE5";
+const PremiumModelAddress = "0x19deD1F547947dE7367A8264cD76CFa7d9702BC0";
+const ParametersAddress = "0x2CeE0019b7aeE5d3A5F1db141f0f2F7769d021EA";
+const VaultAddress = "0xd48C923681a9DEC5CE1F032C2204c0fcFDc2C0B0";
+
+const PoolTemplateAddress = "0xDF95eEFea6F322742c64D05AEC124a334C1557BA";
+
+const Pools = ["0x46c08576bf9c5E821780F24812817b809ba65D80"];
+
+export {
+  USDCAddress,
+  OwnershipAddress,
+  RegistryAddress,
+  FactoryAddress,
+  PremiumModelAddress,
+  ParametersAddress,
+  VaultAddress,
+  PoolTemplateAddress,
+  Pools,
+};
